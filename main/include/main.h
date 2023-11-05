2ed97619c8a38eb15de58db12e35c121a7b4f89f
L.J <109549349+JuraszekL@users.noreply.github.com>
2023-11-05 20:47:53 +0100

    changed version to v1.1.0

--- main/include/main.h
+++ main/include/main.h
@@ -82,7 +82,7 @@ typedef struct {
  * Project version
  ***************************************************************/
 #define VERSION_MAJOR		"1"
-#define VERSION_MINOR		"0"
+#define VERSION_MINOR		"1"
 #define VERSION_PATCH		"0"
 
 /**************************************************************
